d5732b3066ac02862ef02270adb99e334c42bbcc
azurim04 <alopezc1904@alumno.ipn.mx>
2021-09-14 23:22:16 -0500

    css 2

---
+++ 01 Cesar/js/cesar.js
@@ -0,0 +1,10 @@
+var cesar = cesar || (function(){
+    var proceso = function(txt, desp, action){
+        var replace = (function(){
+            var abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+                        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
+                        'x', 'y', 'z'];
+            var l = abc.length;
+        })
+    }
+})
