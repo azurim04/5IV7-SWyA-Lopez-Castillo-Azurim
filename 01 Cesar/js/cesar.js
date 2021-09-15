2b1927019704df05a8acb3e4c3bc47f77f0bf38e
azurim04 <alopezc1904@alumno.ipn.mx>
2021-09-14 23:29:27 -0500

    logica cesar

--- 01 Cesar/js/cesar.js
+++ 01 Cesar/js/cesar.js
@@ -5,6 +5,45 @@ var cesar = cesar || (function(){
                         'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
                         'x', 'y', 'z'];
             var l = abc.length;
-        })
-    }
-})
+
+            return function(c){
+                var i = abc.indexOf(c.toLowerCase());
+                if(i != -1){
+                    var pos = i;
+                    if(action){
+                        pos += desp;
+                        pos -= (pos >= l)?l:0;
+                    }else{
+                        pos -= desp;
+                        pos += (pos < 0)?l:0;
+                    }
+                    return abc[pos];
+                }
+                return c;
+            };
+        })();
+        var re = (/([a-z])/ig);
+        return String(txt).replace(re, function(match){
+            return replace(match);
+        });
+    };
+
+    return{
+        encode : function(txt, desp){
+            return proceso(txt, desp, true);
+        },
+        decode : function(txt, desp){
+            return proceso(txt, desp, false);
+        }
+    };
+})();
+
+function cifrar(){
+    document.getElementById("resultado").innerHTML =
+    cesar.encode(document.getElementById("cadena").value, 3);
+}
+
+function descifrar(){
+    document.getElementById("resultado").innerHTML =
+    cesar.decode(document.getElementById("cadena").value, 3);
+}
